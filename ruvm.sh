9d53465f0609c021a78f5e16a89be275d676f213
Interfiber <hunterstasonis@icloud.com>
2021-02-11 09:37:38 -0500

    Update version to 1.50.0

--- ruvm.sh
+++ ruvm.sh
@@ -116,7 +116,7 @@ if [[ $1 == "install" ]]; then
   version=$2
   if [[ $version == "--lts" ]]; then
     # Set Version to the latest if user wants latest!
-    version="1.49.0"
+    version="1.50.0"
   fi
   echo "Rust Install Version : $version" >> log
   PACKAGE_URL=$(GenerateUrl $version)
